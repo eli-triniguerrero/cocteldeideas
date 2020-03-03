fbf51465a6583a6b80470a2a3273f06bca9c81b5
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-03 17:41:58 -0600

    estilos del navbar listos, faltan ajustar detalles de alineacion

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,11 +1,5 @@
 // console.log('Hola desde la consola');
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//   });
-
-  // Or with jQuery
-
-//   $(document).ready(function(){
-//     $('.sidenav').sidenav();
-//   });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
